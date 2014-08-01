3a607137197042a28d66108c8c3357474c9589db
Jose Contreras <jocontre@joselcontreras.com>
2014-08-01 01:29:46 -0700

    Progress

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var app = angular.module('Todo',[]);
-app.controller('TodoController', function($scope){
-
+app.controller('TodoCtrl', function($scope){
+	// $scope (an object) is the link between this controller and to the view!
+	// Also. stick to naming conventions, such as Controllers as Ctrl
+	$scope.todos = ['Learn to play a 12 stringer',
+					'Go to Japan for 2020 Olympics',
+					'Get a good job!'];
 });
